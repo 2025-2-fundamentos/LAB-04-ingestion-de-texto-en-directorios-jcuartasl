1a14c3824ba15e16951539968c1262217d88d771
jcuartasl <jcuartasl@unal.edu.co>
2025-12-04 00:02:20 -0500

    finale?

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,15 +1,7 @@
-# pylint: disable=import-outside-toplevel
-# pylint: disable=line-too-long
-# flake8: noqa
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
-
-
-from glob import glob
-
-
 def pregunta_01():
+    import os
+    import zipfile
+    import pandas as pd
     """
     La información requerida para este laboratio esta almacenada en el
     archivo "files/input.zip" ubicado en la carpeta raíz.
@@ -74,43 +66,127 @@ def pregunta_01():
 
 
     """
-    import os, glob
+    with zipfile.ZipFile("files/input.zip", "r") as zip_ref:
+        zip_ref.extractall("files")
+
+    test = []
+    for folder in ["test"]:
+        for sentiment in ["negative", "positive", "neutral"]:
+            for file in os.listdir(f"files/input/{folder}/{sentiment}"):
+                with open(f"files/input/{folder}/{sentiment}/{file}", "r") as f:
+                    test.append({"phrase": f.read(), "target": sentiment})
+
+    train = []
+    for folder in ["train"]:
+        for sentiment in ["negative", "positive", "neutral"]:
+            for file in os.listdir(f"files/input/{folder}/{sentiment}"):
+                with open(f"files/input/{folder}/{sentiment}/{file}", "r") as f:
+                    train.append({"phrase": f.read(), "target": sentiment})
+
+    df_test = pd.DataFrame(test)
+    df_train = pd.DataFrame(train)
+
+    os.makedirs("files/output", exist_ok=True)
+    df_train.to_csv("files/output/train_dataset.csv", index=False)
+    df_test.to_csv("files/output/test_dataset.csv", index=False)
+
+pregunta_01()# pylint: disable=import-outside-toplevel
+# pylint: disable=line-too-long
+# flake8: noqa
+"""
+Escriba el codigo que ejecute la accion solicitada en cada pregunta.
+"""
+
+
+def pregunta_01():
+    import os
+    import zipfile
     import pandas as pd
+    """
+    La información requerida para este laboratio esta almacenada en el
+    archivo "files/input.zip" ubicado en la carpeta raíz.
+    Descomprima este archivo.
+
+    Como resultado se creara la carpeta "input" en la raiz del
+    repositorio, la cual contiene la siguiente estructura de archivos:
 
 
-    def read_data(sub, folder):
-        data = []
-        for file in glob.glob(f".\\files\\input\\{sub}\\{folder}\\*.txt"):
-            with open(file, "r", encoding="utf-8") as f:
-                text = f.read().strip()
-            data.append((text, folder))
-        return data
-    
+    ```
+    train/
+        negative/
+            0000.txt
+            0001.txt
+            ...
+        positive/
+            0000.txt
+            0001.txt
+            ...
+        neutral/
+            0000.txt
+            0001.txt
+            ...
+    test/
+        negative/
+            0000.txt
+            0001.txt
+            ...
+        positive/
+            0000.txt
+            0001.txt
+            ...
+        neutral/
+            0000.txt
+            0001.txt
+            ...
+    ```
 
-    df_train = pd.DataFrame(
-        read_data("train", "negative") +
-        read_data("train", "positive") +
-        read_data("train", "neutral"),
-        columns=["phrase", "target"]
-    )
+    A partir de esta informacion escriba el código que permita generar
+    dos archivos llamados "train_dataset.csv" y "test_dataset.csv". Estos
+    archivos deben estar ubicados en la carpeta "output" ubicada en la raiz
+    del repositorio.
 
-    df_test = pd.DataFrame(
-        read_data("test", "negative") +
-        read_data("test", "positive") +
+    Estos archivos deben tener la siguiente estructura:
+
+    * phrase: Texto de la frase. hay una frase por cada archivo de texto.
+    * sentiment: Sentimiento de la frase. Puede ser "positive", "negative"
+      o "neutral". Este corresponde al nombre del directorio donde se
+      encuentra ubicado el archivo.
+
+    Cada archivo tendria una estructura similar a la siguiente:
+
+    ```
+    |    | phrase                                                                                                                                                                 | target   |
+    |---:|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------|:---------|
+    |  0 | Cardona slowed her vehicle , turned around and returned to the intersection , where she called 911                                                                     | neutral  |
+    |  1 | Market data and analytics are derived from primary and secondary research                                                                                              | neutral  |
+    |  2 | Exel is headquartered in Mantyharju in Finland                                                                                                                         | neutral  |
+    |  3 | Both operating profit and net sales for the three-month period increased , respectively from EUR16 .0 m and EUR139m , as compared to the corresponding quarter in 2006 | positive |
+    |  4 | Tampere Science Parks is a Finnish company that owns , leases and builds office properties and it specialises in facilities for technology-oriented businesses         | neutral  |
+    ```
 
 
-        
-        read_data("test", "neutral"),
-        columns=["phrase", "target"]
-    )
+    """
+    with zipfile.ZipFile("files/input.zip", "r") as zip_ref:
+        zip_ref.extractall("files")
 
+    test = []
+    for folder in ["test"]:
+        for sentiment in ["negative", "positive", "neutral"]:
+            for file in os.listdir(f"files/input/{folder}/{sentiment}"):
+                with open(f"files/input/{folder}/{sentiment}/{file}", "r") as f:
+                    test.append({"phrase": f.read(), "target": sentiment})
 
-    if os.path.exists("files/output/"):
-        for file in glob.glob(f"files/output/*"):
-            os.remove(file)
-    else:
-        os.makedirs("files/output")
+    train = []
+    for folder in ["train"]:
+        for sentiment in ["negative", "positive", "neutral"]:
+            for file in os.listdir(f"files/input/{folder}/{sentiment}"):
+                with open(f"files/input/{folder}/{sentiment}/{file}", "r") as f:
+                    train.append({"phrase": f.read(), "target": sentiment})
 
+    df_test = pd.DataFrame(test)
+    df_train = pd.DataFrame(train)
+
+    os.makedirs("files/output", exist_ok=True)
     df_train.to_csv("files/output/train_dataset.csv", index=False)
     df_test.to_csv("files/output/test_dataset.csv", index=False)
 
